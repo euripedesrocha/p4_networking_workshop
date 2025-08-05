58258ef18f3ccf899cc583c311603addda593b93
Euripedes <rocha.euripedes@gmail.com>
2025-08-05 16:48:28 -0300

    Initial commit

---
+++ global-top.vue
@@ -0,0 +1,15 @@
+<template>
+  <header
+    style="
+      position: fixed;
+      top: 0.5rem;
+      right: 1rem;
+      width: 180px;
+      opacity: 0.8;
+      pointer-events: none;
+      z-index: 9999;
+    "
+  >
+    <img src="/espressif-logos/Espressif_Standard_Logo_EN_Horizontal_Black.svg" alt="Espressif Logo" style="width: 100%;" />
+  </header>
+</template>
